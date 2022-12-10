61140c7a4a26b9da3a4330dad0bf55c432dab979
Ibranista <ibranista88@gmail.com>
2022-12-10 14:25:25 +0100

    updated readme file.

---
+++ src/modules/target-DOM.js
@@ -0,0 +1,5 @@
+export const Description = document.getElementById('work');
+export const Notification = document.querySelector('.show-msg');
+export const Submit = document.querySelector('.clear-lists');
+export const ListWrapper = document.querySelector('.items-list-wrapper');
+export const 
