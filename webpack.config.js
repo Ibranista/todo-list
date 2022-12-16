ade94e7ca9ec6d0a63a964a4daf9d318f71d145a
Ibranista <ibranista88@gmail.com>
2022-12-16 09:14:10 +0100

    fixed linter issues.

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
