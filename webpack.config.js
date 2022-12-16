944166fbe9da359f81b78683f28ec43dc42989a2
Ibranista <ibranista88@gmail.com>
2022-12-16 09:19:40 +0100

    converted to es6 module.

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,51 @@
 /* eslint-disable linebreak-style */
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const path = require('path');
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin, { loader as _loader } from 'mini-css-extract-plugin';
+import { resolve } from 'path';
 
-module.exports = {
-  mode: 'development',
-  entry: {
-    index: './src/index.js',
-  },
-  devServer: {
-    static: './dist',
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    assetModuleFilename: 'images/[name][ext]',
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.(s[ac]|c)ss$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-      {
-        test: /\.js$/i,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
+export const mode = 'development';
+export const entry = {
+  index: './src/index.js',
+};
+export const devServer = {
+  static: './dist',
+};
+export const output = {
+  filename: '[name].bundle.js',
+  path: resolve(__dirname, 'dist'),
+  clean: true,
+  assetModuleFilename: 'images/[name][ext]',
+};
+export const module = {
+  rules: [
+    {
+      test: /\.(s[ac]|c)ss$/i,
+      use: [_loader, 'css-loader'],
+    },
+    {
+      test: /\.js$/i,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
         },
       },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Webpack Setup',
-      template: path.resolve(__dirname, 'src', 'index.html'),
-    }),
-    new MiniCssExtractPlugin(),
+    },
+    {
+      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      type: 'asset/resource',
+    },
   ],
-  devtool: 'inline-source-map',
-
-  optimization: {
-    runtimeChunk: 'single',
-  },
+};
+export const plugins = [
+  new HtmlWebpackPlugin({
+    title: 'Webpack Setup',
+    template: resolve(__dirname, 'src', 'index.html'),
+  }),
+  new MiniCssExtractPlugin(),
+];
+export const devtool = 'inline-source-map';
+export const optimization = {
+  runtimeChunk: 'single',
 };
